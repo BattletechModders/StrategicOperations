cf21b45e053a182c0759917712d8a4ea37d09fe2
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2021-11-15 07:42:26 -0500

    probably fix for dialogue + objectives issues

--- StrategicOperations/StrategicOperations/Patches/DirtyDebugPatches.cs
+++ StrategicOperations/StrategicOperations/Patches/DirtyDebugPatches.cs
@@ -6,13 +6,81 @@ using System.Threading.Tasks;
 using BattleTech;
 using CustAmmoCategories;
 using CustomAmmoCategoriesPatches;
+using FogOfWar;
 using Harmony;
+using HBS;
+using UnityEngine;
 
 namespace StrategicOperations.Patches
 {
     class DirtyDebugPatches
     {
-        [HarmonyPatch(typeof(ReserveActorInvocation), "Invoke", new Type[] { typeof(CombatGameState) })]
+        [HarmonyPatch(typeof(MultiSequence), "RevealRadius", new Type[] {typeof(Vector3), typeof(float), typeof(string)})]
+        public static class MultiSequence_RevealRadius_GrossPatchReplacer
+        {
+            public static bool Prefix(MultiSequence __instance, Vector3 focalPosition, float revealRadius, string parentGUID, ref GameObject ___focalPoint, ref List<PilotableActorRepresentation> ___shownList, ref int ___nextRevealatronIndex)
+            {
+                if (revealRadius != 0f)
+                {
+                    var combat = UnityGameInstance.BattleTechGame.Combat;
+                    //__instance.ClearFocalPoint();
+                    if (___focalPoint != null)
+                    {
+                        UnityEngine.Object.Destroy(___focalPoint);
+                        ___focalPoint = null;
+                    }
+
+                    ___focalPoint = new GameObject("focalPoint");
+                    SnapToTerrain snapToTerrain = ___focalPoint.AddComponent<SnapToTerrain>();
+                    snapToTerrain.verticalOffset = 10f;
+                    snapToTerrain.transform.position = focalPosition;
+                    snapToTerrain.UpdatePosition();
+                    FogOfWarRevealatron fogOfWarRevealatron = ___focalPoint.AddComponent<FogOfWarRevealatron>();
+
+                    ___nextRevealatronIndex++;
+                    fogOfWarRevealatron.GUID = parentGUID + string.Format(".{0}", ___nextRevealatronIndex);
+                    fogOfWarRevealatron.radiusMeters = revealRadius;
+                    LazySingletonBehavior<FogOfWarView>.Instance.FowSystem.AddRevealatronViewer(fogOfWarRevealatron);
+                    List<AbstractActor> allActors = combat.AllActors;
+                    //__instance.ClearShownList();
+
+                    if (___shownList != null)
+                    {
+                        List<ICombatant> allCombatants = combat.GetAllCombatants();
+                        for (int i = 0; i < ___shownList.Count; i++)
+                        {
+                            ___shownList[i].ClearForcedPlayerVisibilityLevel(allCombatants);
+                        }
+                        ___shownList = null;
+                    }
+
+                    ___shownList = new List<PilotableActorRepresentation>();
+                    for (int i = 0; i < allActors.Count; i++)
+                    {
+                        AbstractActor abstractActor = allActors[i];
+                        UnitSpawnPointGameLogic itemByGUID = combat.ItemRegistry.GetItemByGUID<UnitSpawnPointGameLogic>(abstractActor.spawnerGUID);
+                        var position = new Vector3(9999.9f, 0f, 0f);
+                        if (itemByGUID != null)
+                        {
+                            position = itemByGUID.Position;
+                        }
+
+                        if (!allActors[i].team.IsLocalPlayer && (Vector3.Distance(allActors[i].CurrentPosition, focalPosition) < revealRadius || (abstractActor.IsTeleportedOffScreen && Vector3.Distance(position, focalPosition) < revealRadius)))
+                        {
+                            PilotableActorRepresentation pilotableActorRepresentation = abstractActor.GameRep as PilotableActorRepresentation;
+                            if (pilotableActorRepresentation != null)
+                            {
+                                pilotableActorRepresentation.SetForcedPlayerVisibilityLevel(VisibilityLevel.LOSFull, false);
+                                ___shownList.Add(pilotableActorRepresentation);
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(ReserveActorInvocation), "Invoke", new Type[] { typeof(CombatGameState)})]
         public static class ReserveActorInvocation_Invoke_ShittyBypass
         {
             static bool Prepare() => true; //enabled
